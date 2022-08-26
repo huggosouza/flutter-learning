059d7a4062cfc9541c886950d4613efa50ae5adb
Hugo Souza <hugoedson2019@gmail.com>
2022-08-26 10:05:15 -0300

    Update imc.dart

--- imccalculator/imc.dart
+++ imccalculator/imc.dart
@@ -12,10 +12,31 @@ main(){
   var height = stdin.readLineSync();
   var dHeight = double.parse(height!);
 
-/* ------------------ CALC ---------------------------*/
+/* ------------------- CALC ------------------------- */
+  var calc = (dWeight / (dHeight * dHeight)).truncateToDouble();
+  print("== Your IMC is: $calc");
 
-  var calc = dWeight / (dHeight * dHeight);
-  
-
-  print("== Your IMC is: ${calc}");
+  if (calc <= 16.0) {
+    print("Low weight level 3");
+  } else if(calc <= 16.9) {
+    print("Low weight level 2");
+  }
+  else if(calc <= 18.5){
+    print("Low weight level 1");
+  }
+  else if(calc <= 24.9){
+    print("Ideal weight");
+  }
+  else if(calc > 25.0){
+    print("Overweight");
+  }
+  else if(calc > 30.0){
+    print("Obesity level 1");
+  }
+  else if(calc > 35.0){
+    print("Obesity level 2");
+  }
+  else if(calc > 39.9){
+    print("Obesity level 3");
+  }
 }
