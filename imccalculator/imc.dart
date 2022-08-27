f28908a74d2173ed62e20f7d0866eb29e5020550
Hugo Souza <hugoedson2019@gmail.com>
2022-08-27 19:50:59 -0300

    with functions now

--- imccalculator/imc.dart
+++ imccalculator/imc.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 main(){
+  printResult(calculoDeIMC());
+}
 
-/* ------------------ WEIGHT -------------------------*/
+calculoDeIMC(){
+  /* ------------------ WEIGHT -------------------------*/
   print("=== Type your weight: ");
   var weight = stdin.readLineSync();
   var dWeight = double.parse(weight!);
@@ -14,29 +17,40 @@ main(){
 
 /* ------------------- CALC ------------------------- */
   var calc = (dWeight / (dHeight * dHeight)).truncateToDouble();
-  print("== Your IMC is: $calc");
 
+  return calc;
+}
+
+printResult(calc){
   if (calc <= 16.0) {
+    print("== Your IMC is: $calc");
     print("Low weight level 3");
   } else if(calc <= 16.9) {
+    print("== Your IMC is: $calc");
     print("Low weight level 2");
   }
   else if(calc <= 18.5){
+    print("== Your IMC is: $calc");
     print("Low weight level 1");
   }
   else if(calc <= 24.9){
+    print("== Your IMC is: $calc");
     print("Ideal weight");
   }
   else if(calc > 25.0){
+    print("== Your IMC is: $calc");
     print("Overweight");
   }
   else if(calc > 30.0){
+    print("== Your IMC is: $calc");
     print("Obesity level 1");
   }
   else if(calc > 35.0){
+    print("== Your IMC is: $calc");
     print("Obesity level 2");
   }
   else if(calc > 39.9){
+    print("== Your IMC is: $calc");
     print("Obesity level 3");
   }
 }
