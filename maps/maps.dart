77da07d301e6b8e1720e3eb45c13c07aa88750d3
Hugo Souza <hugoedson2019@gmail.com>
2022-09-02 09:33:17 -0300

    Update maps.dart

--- maps/maps.dart
+++ maps/maps.dart
@@ -1,4 +1,44 @@
-void main(List<String> args) {
-  var maps = {"name": args[0], "age": args[1], "gender": args[2]};
-  print(maps);
+import 'dart:io';
+
+Map<String, dynamic> cadastre = {};
+void main() {
+  bool condition = true;
+  while (condition = true) {
+    print("Type your command to the system: ");
+    String command = stdin.readLineSync()!;
+    if (command == "exit") {
+      exit(0);
+    } else if (command == "register") {
+      clearTerm();
+      register();
+    } else if (command == "print") {
+      clearTerm();
+      print(cadastre);
+    } else {
+      print("Invalid command!");
+    }
+  }
+}
+
+void register() {
+  clearTerm();
+  print("Type your name: ");
+  cadastre["name"] = stdin.readLineSync();
+
+  clearTerm();
+  print("Type your age: ");
+  cadastre["age"] = stdin.readLineSync();
+
+  clearTerm();
+  print("Type your town: ");
+  cadastre["town"] = stdin.readLineSync();
+
+  clearTerm();
+  print("Type your state: ");
+  cadastre["state"] = stdin.readLineSync();
+  ;
+}
+
+void clearTerm() {
+  print("\x1B[2J\x1B[0;0H");
 }
